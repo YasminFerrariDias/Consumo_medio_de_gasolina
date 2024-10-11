243118b6cf1f557611d650a8ea277939b86427d3
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2024-10-10 21:07:51 -0300

    Add files via upload

---
+++ ConsumoMedioDeGasolina.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ConsumoMedioDeGasolina {
+  public static void main(String[] args) {
+    Scanner var = new Scanner(System.in);
+    float dist, combs, cons;
+    System.out.print("Digite a distância percorrida pelo automóvel em quilômetros: ");
+    dist = var.nextFloat();
+    System.out.print("Digite o total de combustivel gasto: ");
+    combs = var.nextFloat();
+    cons = dist/combs;
+    System.out.print("O consumo médio é de " + cons + " km por litro.");
+  }   
+}
